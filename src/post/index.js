b26e1458db4dad3d3c90d2ce045640b851715bc2
magdakolaniak <magda.kolaniak@gmail.com>
2021-06-14 12:10:58 +0200

    commit

---
+++ src/post/index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import postModel from './schema.js';
+
+const postRouter = express.Router();
+
+postRouter.get('/', async (req, res, next) => {
+  try {
+    const posts = await postModel.find();
+    res.send(posts);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+postRouter.get('/:id', async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel.findById(postId);
+    res.send(post);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+postRouter.post('/', async (req, res, next) => {
+  try {
+    const newPost = new postModel(req.body);
+    const data = await newPost.save();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+postRouter.put('/:id', async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel.findByIdAndUpdate(postId, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.send(post);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+postRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel.findByIdAndDelete(postId);
+    
+
+    if (post) {
+      res.status(204).send('Sucesfully deleted!');
+    } else {
+      res.send('Post with given ID not found');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default postRouter;
